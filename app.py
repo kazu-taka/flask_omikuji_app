c20fac85b7366a67255365fee70f4a1d02df720e
kazu-taka <k.takahashi@next-revolution.net>
2026-07-19 16:47:34 +0900

    おみくじアプリ完成

---
+++ app.py
@@ -0,0 +1,15 @@
+import random
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# おみくじアプリルーティング
+@app.route("/omikuji")
+def omikuji():
+    result = random.choice(["大吉!!", "吉", "凶..."])
+    return render_template("omikuji.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
